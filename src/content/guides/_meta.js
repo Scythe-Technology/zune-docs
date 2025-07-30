3a62367ebf80b0a847eb03f69bf82e24e117e0de
Sythivo <69376875+Sythivo@users.noreply.github.com>
2025-07-30 00:48:37 -0400

    add guides

---
+++ src/content/guides/_meta.js
@@ -0,0 +1,14 @@
+export default {
+    index: {
+        type: 'doc',
+        title: 'Guides',
+    },
+    install: {
+        type: 'doc',
+        title: 'Installation',
+    },
+    '---1': {
+        "type": "separator"
+    },
+    // optimization: "Optimization",
+}
